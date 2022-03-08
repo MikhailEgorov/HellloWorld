3f63e725036258b63eaa82c4d1f4b6a35a2a162e
MikhailEgorov <egorovmail25gmail.com>
2022-03-08 15:27:28 +0300

    Initial Commit

---
+++ HellloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HellloWorld
+//
+//  Created by Егоров Михаил on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
